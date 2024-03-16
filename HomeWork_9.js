9ad7fee0b0b646c5ee0fcdff95632328e66b5663
Houston <144223510+Eduard-Babchuk@users.noreply.github.com>
2024-03-17 01:26:24 +0200

    Fix

--- HomeWork_9.js
+++ HomeWork_9.js
@@ -1,25 +1,32 @@
 const fs = require('fs')
-const readline = require('readline')
-const filePath = 'ImRobot.txt'
 
+const filePath = 'ImRobot.txt'
 let totalWords = 0
+
+const readStream = fs.createReadStream(filePath, { encoding: 'latin1' })
+
+readStream.on('data', (chunk) => {
+    const chunkString = chunk.toString('utf8')
+    const cleanChunk = stripAnsi(chunkString)
+    const lines = cleanChunk.split('\n')
+    
+    lines.forEach((line) => {
+        console.log(line)
+        totalWords += countWords(line)
+    })
+})
+
+readStream.on('end', () => {
+    console.log(`Total words: ${totalWords}`)
+})
 
 function countWords(line) {
     const wordRegex = /\b\w+\b/g
     return (line.match(wordRegex) || []).length
 }
 
-const reader = readline.createInterface({
-    input: fs.createReadStream(filePath, { encoding: 'latin1' }),
-    output: process.stdout,
-    terminal: false
-})
+function stripAnsi(text) {
+    const ansiRegex = /\x1B\[[0-?]*[ -/]*[@-~]/g
+    return text.replace(ansiRegex, '')
+}
 
-reader.on('line', (line) => {
-    console.log(line)
-    totalWords += countWords(line)
-})
-
-reader.on('close', () => {
-    console.log(`Total words: ${totalWords}`)
-})
